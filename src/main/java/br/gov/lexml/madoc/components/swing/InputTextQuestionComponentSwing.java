2d8788b8b6757926fafe7f48635aa7fd238b8e9d
fragomeni <marcos.fragomeni@gmail.com>
2013-10-29 17:08:20 -0200

    Implementação da utilização do atributo regex no InputTextQuestionComponentSwing

--- src/main/java/br/gov/lexml/madoc/components/swing/InputTextQuestionComponentSwing.java
+++ src/main/java/br/gov/lexml/madoc/components/swing/InputTextQuestionComponentSwing.java
@@ -1,15 +1,25 @@
 package br.gov.lexml.madoc.components.swing;
 
 import java.awt.event.ActionListener;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import javax.swing.JTextField;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.DocumentFilter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import br.gov.lexml.madoc.components.ComponentController;
 import br.gov.lexml.madoc.components.swing.jcomponents.QuestionPanel;
 import br.gov.lexml.madoc.schema.entity.InputTextQuestionType;
 import br.gov.lexml.madoc.schema.entity.QuestionAnswerType;
+import br.gov.lexml.swing.util.PatternDocumentFilter;
 
 class InputTextQuestionComponentSwing extends AbstractQuestionComponentSwing<InputTextQuestionType> {
+	
+	private static final Log log = LogFactory.getLog(InputTextQuestionComponentSwing.class);
 
 	private JTextField textField;
 	
@@ -45,11 +55,22 @@ class InputTextQuestionComponentSwing extends AbstractQuestionComponentSwing<Inp
 
 		textField = new JTextField();
 
+		if(wizardElement.isSetRegex()) {
+			try {
+				Pattern pattern = Pattern.compile(wizardElement.getRegex());
+				DocumentFilter docFilter = new PatternDocumentFilter(pattern);
+				((AbstractDocument)textField.getDocument()).setDocumentFilter(docFilter);
+			}
+			catch(PatternSyntaxException e) {
+				log.error("Pattern '" + wizardElement.getRegex() + "' inválida.");
+			}
+		}
+
 		if (wizardElement.isSetDefaultValue()) {
 			// setar valor default depois do addListener
 			textField.setText(hostEditorReplacer.replaceString(wizardElement.getDefaultValue()));
 		}
-
+		
 		//add listener
 		ActionListener listener = createDefaultListener();
 		if (listener!= null){
