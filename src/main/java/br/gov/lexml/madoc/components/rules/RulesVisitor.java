249bfd3b4c82e408b6ec3f52c7064abc25488d4b
fragomeni <marcos.fragomeni@gmail.com>
2015-04-20 10:01:21 -0300

    Possibilidade de selecionar SwitchRule pelo id da opção selecionada na questão.

--- src/main/java/br/gov/lexml/madoc/components/rules/RulesVisitor.java
+++ src/main/java/br/gov/lexml/madoc/components/rules/RulesVisitor.java
@@ -6,6 +6,8 @@ import br.gov.lexml.madoc.components.QuestionComponent;
 import br.gov.lexml.madoc.schema.Constants;
 import br.gov.lexml.madoc.schema.entity.ActionListType;
 import br.gov.lexml.madoc.schema.entity.ActionType;
+import br.gov.lexml.madoc.schema.entity.QuestionAnswerOptionType;
+import br.gov.lexml.madoc.schema.entity.QuestionAnswerOptionsType;
 import br.gov.lexml.madoc.schema.entity.QuestionAnswerType;
 import br.gov.lexml.madoc.schema.entity.SimpleRuleType;
 import br.gov.lexml.madoc.schema.entity.SwitchCaseRuleType;
@@ -15,17 +17,17 @@ import br.gov.lexml.madoc.schema.entity.visitor.VisitorAction;
 
 /**
  * Implement each type of RuleType (Visitor pattern)
- * 
+ *
  * @author lauro
- * 
+ *
  */
 public class RulesVisitor extends BaseVisitor {
 
 	private final QuestionComponent<?,?> questionComponent;
 	private final ComponentController componentController;
-	
+
 	/**
-	 * RulesVisitor for a Wizard Rules 
+	 * RulesVisitor for a Wizard Rules
 	 * @param componentController
 	 * @param when
 	 */
@@ -33,14 +35,14 @@ public class RulesVisitor extends BaseVisitor {
 		this.questionComponent = null;
 		this.componentController = componentController;
 	}
-	
+
 	public RulesVisitor(QuestionComponent<?,?> questionComponent, ComponentController componentController){
 		this.questionComponent = questionComponent;
 		this.componentController = componentController;
 	}
 
 	/**
-	 * RulesVisitor for a CommandComponent. Executes only BaseRuleWhenType.ON_CHANGE rules. 
+	 * RulesVisitor for a CommandComponent. Executes only BaseRuleWhenType.ON_CHANGE rules.
 	 * @param commandComponent
 	 * @param componentController
 	 */
@@ -54,29 +56,29 @@ public class RulesVisitor extends BaseVisitor {
 	 *  - it always executes
 	 */
 	public VisitorAction enter(SimpleRuleType aBean) {
-		
+
 		ActionVisitor actionVisitor = new ActionVisitor(componentController);
-			
+
 		for (ActionType action : aBean.getAction()){
 			action.accept(actionVisitor);
 		}
-		
+
 		return VisitorAction.CONTINUE;
 	}
-	
+
 	/**
 	 * SwitchRuleType has the following semantic:
 	 *  - the questionValue might have multiple values, separated by commas (,), like in questionValue="x,y,z"
 	 * 	- each CASE (of the type ActionListType) is executed when all of its questionValue="x,y,z" are true
 	 *  - the OTHERWISE (of the type ActionListType) is executed when no CASE are true
 	 * When SwitchRuleType is set on a CommandType, SwitchRuleType is executed only when questionId is set
-	 *   
+	 *
 	 */
 	@Override
 	public VisitorAction enter(SwitchRuleType aBean) {
 
 			// getting Question
-			
+
 			final QuestionComponent<?,?> wizardElement;
 			if (!aBean.isSetQuestionId()){
 				wizardElement = this.questionComponent;
@@ -87,53 +89,70 @@ public class RulesVisitor extends BaseVisitor {
 					wizardElement = componentController.getQuestionComponentById(aBean.getQuestionId());
 				}
 			}
-			
+
 			if (wizardElement== null){
 				return VisitorAction.CONTINUE;
 			}
-	
+
 			// getting component answer
-			
+
 			QuestionAnswerType answer = wizardElement.getAnswer();
-			
+
 			if (answer== null){
 				return VisitorAction.CONTINUE;
 			}
-			
+
 			String componentValue = answer.getValue();
-	
+
 			// if valueFound = true, there's no otherwise
 			boolean valueFound = false;
-	
+
 			// for each case
 			for (ActionListType action : aBean.getCase()) {
-	
+
 				// getting question value
 				String questionValue = ((SwitchCaseRuleType) action).getQuestionValue();
-	
+				String attributeToTest = ((SwitchCaseRuleType) action).getAttributeToTest();
+
 				boolean allSelectedExternal = true;
-				for (String questionOneValue : questionValue.split(Constants.SPLIT_TOKEN_VALUES)) {
-					boolean internalFound = false;
-					for (String componentValueSelected : componentValue.split(Constants.SPLIT_TOKEN_VALUES)) {
-	
-						if (questionOneValue.trim().equals(componentValueSelected.trim())) {
-							internalFound = true;
-	
+				if(attributeToTest.equals("optionId")) {
+
+					String selectedOptionId = "";
+					QuestionAnswerOptionsType options = answer.getOptions();
+					for(QuestionAnswerOptionType o: options.getOption()) {
+						if(o.isSelected()) {
+							selectedOptionId = o.getId();
 							break;
 						}
 					}
-					if (!internalFound) {
-						allSelectedExternal = false;
-						break;
+
+					allSelectedExternal = questionValue.trim().equals(selectedOptionId.trim());
+
+				}
+				else {
+					for (String questionOneValue : questionValue.split(Constants.SPLIT_TOKEN_VALUES)) {
+						boolean internalFound = false;
+						for (String componentValueSelected : componentValue.split(Constants.SPLIT_TOKEN_VALUES)) {
+
+							if (questionOneValue.trim().equals(componentValueSelected.trim())) {
+								internalFound = true;
+
+								break;
+							}
+						}
+						if (!internalFound) {
+							allSelectedExternal = false;
+							break;
+						}
 					}
 				}
 				if (allSelectedExternal) {
 					ActionVisitor actionVisitor = new ActionVisitor(wizardElement.getComponentController());
 					action.accept(actionVisitor);
 					valueFound = true;
-				} 
+				}
 			}
-	
+
 			// treating otherwise
 			if (aBean.isSetOtherwise() && !valueFound) {
 				ActionVisitor actionVisitor = new ActionVisitor(wizardElement.getComponentController());
--- src/test/java/br/gov/lexml/madoc/test/MadocMain.java
+++ src/test/java/br/gov/lexml/madoc/test/MadocMain.java
@@ -33,31 +33,31 @@ public class MadocMain {
 	 */
 
 	// DESENVOLVIMENTO
-	
+
 	private static final String SDLEG_VERSIONED_URL_PATTERN = "http://www6gdsv.senado.gov.br/sdleg-getter/public/getDocument?docverid=$1;$2";
 	private static final String SDLEG_VERSION_LESS_URL_PATTERN = "http://www6gdsv.senado.gov.br/sdleg-getter/public/getDocument?docverid=$1";
 	private static final String SDLEG_CATALOG_RESOURCE_NAME = "urn:sf:sistema;sdleg:id;9110bad3-04aa-4da4-9328-fbae1dd311ed";
-	
-	
-	
+
+
+
 	// HOMOLOGAÇÃO
 	/*private static final String SDLEG_VERSIONED_URL_PATTERN = "http://www6ghml.senado.gov.br/sdleg-getter/public/getDocument?docverid=$1;$2";
 	private static final String SDLEG_VERSION_LESS_URL_PATTERN = "http://www6ghml.senado.gov.br/sdleg-getter/public/getDocument?docverid=$1";
 	private static final String SDLEG_CATALOG_RESOURCE_NAME = "urn:sf:sistema;sdleg:id;e3638f33-2c70-4ea6-8a8b-1416edaf4001";
-*/	
+*/
 
 	private CatalogService catalogService;
-	
+
 	// Cria serviço de catálogo
 	private final String configDirCache = "target/cache";
 	private final String targetRenditions = "target/renditions/";
 	private final String targetSaved = "src/test/testAnswers.xml";
 	//private static final String configDirLocalModels = "src/test/modelos";
 
-	
+
 	{
 		File cache = new File(configDirCache);
-		
+
 		try {
 			if (cache.exists()){
 				FileUtils.deleteDirectory(cache);
@@ -67,7 +67,7 @@ public class MadocMain {
 			System.err.println("Could not delete "+cache.getName());
 			e1.printStackTrace();
 		}
-		
+
 		try {
 			SDLegCatalogServiceBuilder csf = new SDLegCatalogServiceBuilder();
 			csf.setCacheDir(cache)
@@ -76,72 +76,72 @@ public class MadocMain {
 					.setVersionLessUrlPattern(SDLEG_VERSION_LESS_URL_PATTERN);
 
 			catalogService = csf.createCatalogService();
-			
+
 		} catch (CatalogException e) {
 			throw new RuntimeException("Error creating catalog service. "
 					+ e.getMessage(), e);
 		}
 
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 
 		SwingUtilities.invokeAndWait(new Runnable() {
-			
+
 			@Override
 			public void run() {
-				
-				
-				
+
+
+
 				MadocMain madoc = new MadocMain();
 
-				// load document 
+				// load document
 				//madoc.loadMadocDocumentFromLocalDir("m-teste", "1", new File(configDirLocalModels));
 				//madoc.loadMadocDocumentFromSDLeg("m-button-teste");
 				//madoc.loadMadocDocumentFromSDLeg("m-teste");
 				//madoc.loadMadocDocumentFromSDLeg("m016"); // TESTE DO PDF
-				//madoc.loadMadocDocumentFromSDLeg("m001"); 
+				//madoc.loadMadocDocumentFromSDLeg("m001");
 				//madoc.loadMadocDocumentFromSDLeg("cvm001");	// VE TOS
 				//madoc.loadMadocDocumentFromSDLeg("cvm002");	// VETOS COMPLETO
 				//madoc.loadMadocDocumentFromSDLeg("m011");
-				//madoc.loadMadocDocumentFromSDLeg("m016");
-				
+				madoc.loadMadocDocumentFromSDLeg("m026");
+
 				//madoc.showLibraryContent();
-				
+
 				// load answers
 				//madoc.loadMadocAnswers(new File("src/test/testAnswers.xml"));
 				//madoc.loadMadocAnswers(new File("/home/lauroa/Downloads/skeleton-novo.xml"));
-				madoc.loadMadocAnswers(new File("/home/lauroa/Downloads/skeleton-antigo.xml"));
+				//madoc.loadMadocAnswers(new File("/home/lauroa/Downloads/skeleton-antigo.xml"));
 			}
-			
+
 		});
-		
+
 	}
-	
+
 	public void showLibraryContent(){
 		try {
 			MadocLibraryWrapper madocLibraryWrapper = new MadocLibraryWrapper(catalogService, "dataset-library");
-			
+
 			DataSets ds = madocLibraryWrapper.getDataSetsWrapper();
-			
+
 			System.out.println("**** LibraryContent ****");
 			for (Object s : ds.query("/senadores/*/Nome")){
 				System.out.println(s.toString());
 			}
-			
+
 		} catch (CatalogException e) {
 			throw new RuntimeException("Error loading Madoc Library. " + e.getMessage(), e);
-		} 
+		}
 	}
 
 	public void loadMadocAnswers(File f) {
 		try {
 			MadocAnswerType madocAnswers = SchemaParser.loadAnswer(f);
-			
+
 			try{
 				WizardExecutionSwing wExecution = new WizardExecutionSwing(catalogService, madocAnswers);
-				
-				startInterview(wExecution);	
+
+				startInterview(wExecution);
 			} catch (MadocException e) {
 				throw new RuntimeException("Error loading Madoc Document. " + e.getMessage(), e);
 			}
@@ -158,23 +158,23 @@ public class MadocMain {
 	public void loadMadocDocumentFromSDLeg(String id, String version) {
 		// load wizard
 		try {
-			
 
-			
+
+
 			WizardExecutionSwing wExecution = new WizardExecutionSwing(catalogService, id, version);
-			
+
 			startInterview(wExecution);
 		} catch (Exception e) {
 			throw new RuntimeException("Error loading Madoc documents.", e);
 		}
 	}
-	
+
 	public void loadMadocDocumentFromLocalDir(String id, String version, File directory) {
 		try {
 			CatalogServiceFactory csf = new SDLegLocalCatalogServiceBuilder(directory);
 
 			catalogService = csf.createCatalogService();
-			
+
 		} catch (CatalogException e) {
 			throw new RuntimeException("Error creating catalog service. "
 					+ e.getMessage(), e);
@@ -183,13 +183,13 @@ public class MadocMain {
 		// load wizard
 		try {
 			WizardExecutionSwing wExecution = new WizardExecutionSwing(catalogService, id, version);
-			
+
 			startInterview(wExecution);
 		} catch (Exception e) {
 			throw new RuntimeException("Error loading Madoc documents. " + e.getMessage(), e);
 		}
 	}
-	
+
 
 	/**
 	 * Creates default frame
